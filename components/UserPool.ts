b74ab0cad9fa64b6e7c9301cba8e58c13a7636f8
ducnguyen4961 <nguyen@yamamoto-denki.jp>
2025-06-13 10:48:19 +0900

    13/6/25

--- components/UserPool.ts
+++ components/UserPool.ts
@@ -1,8 +1,10 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
+
 const poolData = {
-    UserPoolId: "ap-northeast-1_5RFZ7tKmp",
-    ClientId: "5eid7801fqgv7qu4pjdc7s4pm1",
+  UserPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
+  ClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
 };
+
 
 export default new CognitoUserPool(poolData);
